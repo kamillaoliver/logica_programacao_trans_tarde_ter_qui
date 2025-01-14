ce4d99d3fb568cc31eefafbceac43d9cc7b2b1f7
KAMILLA MELISSA SILVA OLIVEIRA <kamilla.msoliveira@senacsp.edu.br>
2025-01-14 15:23:44 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, mundo! Sou a Kamilla");
+Console.WriteLine("Estou aprendendo lógica de programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá, mundo! Me chamo Kamilla.");
+Console.WriteLine("Estou estudando no Senac Largo Treze.");
