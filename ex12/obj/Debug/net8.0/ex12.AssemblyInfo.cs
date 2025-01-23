6a9054a6bd0cdb553ff9375130387f8a8064509f
kamillaoliver <kamilla.msoliveira@senacsp.edu.br>
2025-01-23 11:38:02 -0300

    exercicio 07

--- ex12/obj/Debug/net8.0/ex12.AssemblyInfo.cs
+++ ex12/obj/Debug/net8.0/ex12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4359967376b9524a1dc7e8e4e43d2b04077370f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0378227c6923eb37e5d035f9f2d7bf2747d0639")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
